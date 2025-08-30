461f708be97c916a6eb7f9a05ccf11a3ca727e7f
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-08-30 22:23:42 +0530

    Update server.py to server.py

--- server.py
+++ server.py
@@ -9,6 +9,6 @@ def home():
     return "ShrutixMusic Bot is running!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
