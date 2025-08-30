20276bb2b7c134df65074dca5d4687ade0e76984
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-08-30 21:51:01 +0530

    Update server.py to server.py

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 # server.py
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "✅ Shrutix Music Bot is alive and running!"
+def home():
+    return "ShrutixMusic Bot is running!"
 
-@app.route("/health")
-def health():
-    return {"status": "ok"}
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
